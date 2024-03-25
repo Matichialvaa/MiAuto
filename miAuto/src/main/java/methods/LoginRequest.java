2b828b95b87a20851051e1f775c78143337e6bfd
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2024-03-25 20:53:10 -0300

    LoginRequest attempt to create passwordValidation() to validate clients account.

--- miAuto/src/main/java/methods/LoginRequest.java
+++ miAuto/src/main/java/methods/LoginRequest.java
@@ -1,7 +1,33 @@
 package methods;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+
 public class LoginRequest {
+    // El SessionFactory que crea las sessiones para hacer los query.
+    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
     // Obtener los mail y contraseña del usuario que quiere acceder
+
+    private boolean passwordValidation( String email ) {
+        // Crea una session para poder hacer el query.
+        Session currentSession = sessionFactory.openSession();
+
+        String SQLquery = "select Password from Userdriver where Email = :input";
+        Query query = currentSession.createSQLQuery(SQLquery).setParameter("input", email);
+
+        // Este paso es para ejecutar el query que esta arriba.
+        List<String> user = query.list();
+        // Es para terminar el uso de la base de datos.
+        currentSession.close();
+
+
+        return user.size() == 1;
+    }
 
     //uso UserDao para chequear si existe el usuario en la base de datos
 
