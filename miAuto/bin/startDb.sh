ec6eb8f99269e316738a97272a61205a522591f1
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-03-26 11:31:04 -0300

    Commit de checkpoint - Previo a intento React EXPO

--- miAuto/bin/startDb.sh
+++ miAuto/bin/startDb.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp ../lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:../db/mydb --dbname.0 xdb
+java -cp C:\Users\flopi\Downloads\hsqldb-2.7.2\hsqldb-2.7.2\hsqldb\lib\hsqldb.jar org.hsqldb.server.Server --database.0 file:C:\Users\flopi\Downloads\hsqldb-2.7.2\hsqldb-2.7.2\hsqldb\db --dbname.0 MiAutodb
