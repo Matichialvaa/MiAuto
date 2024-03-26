2337b9005ad0695f3b7eda3791a43d71a2818417
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-03-26 15:11:18 -0300

    Funcionalidad de paginas implementada

--- miAuto/frontend/ReactFront/App.js
+++ miAuto/frontend/ReactFront/App.js
@@ -1,12 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
+  const handleLoginPress = () => {
+
+    console.log('Login Pressed');
+  };
+
+  const handleRegisterPress = () => {
+    // Logic to navigate to Register screen
+    console.log('Register Pressed');
+  };
+  const Stack = createNativeStackNavigator();
+
+  function HomeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+          <Text>Bienvenido!</Text>
+          <Button
+              title="Log In"
+              onPress={() => navigation.navigate('Login')}
+          />
+          <Button
+              title="Register"
+              onPress={() => navigation.navigate('Register')}
+          />
+        </View>
+    );
+  }
+
+  function LoginScreen() {
+    return (
+        <View style={styles.container}>
+          <Text>Login Screen</Text>
+        </View>
+    );
+  }
+
+  function RegisterScreen() {
+    return (
+        <View style={styles.container}>
+          <Text>Register Screen</Text>
+        </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
@@ -17,4 +66,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
 });
+
